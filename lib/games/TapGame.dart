bdf1ce3b9a536fd22ad461bc2e2601d19e5c1d34
taivan <enkhtaivan1103@gmail.com>
2023-11-17 17:16:48 +0900

    Update

--- lib/games/TapGame.dart
+++ lib/games/TapGame.dart
@@ -2,8 +2,8 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:google_fonts/google_fonts.dart';
-import '/startPage.dart';
 import 'package:audioplayers/audioplayers.dart';
+import '/startPage.dart';
 
 void main() => runApp(const TapGame());
 
@@ -32,13 +32,17 @@ class _GamePageState extends State<GamePage>
   late Animation<double> _animation;
   double _top = 0.0;
   double _left = 0.0;
-  final double _right = 0.0;
-  double _fallingSpeed = 1.0;
+  double _fallingSpeed = 5.0;
   String _fallSpeedCounter = '0.0';
   int _score = 0;
   int _heart = 5;
+  int _try = 2;
+  int _calculatedScore = 0;
   final Random _random = Random();
   int _randomIndex = 0;
+
+  String _backgroundImg = 'lib/assets/bg_3.gif';
+  bool _stopped = false;
 
   final double _width = 80;
   final double _height = 80;
@@ -50,7 +54,7 @@ class _GamePageState extends State<GamePage>
     super.initState();
 
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 500),
+      duration: const Duration(milliseconds: 3000),
       vsync: this,
     );
 
@@ -64,10 +68,11 @@ class _GamePageState extends State<GamePage>
             _left = _random.nextDouble() * MediaQuery.of(context).size.width;
             // _right = _random.nextDouble() * MediaQuery.of(context).size.width;
             _heart -= 1;
+            _player.play(AssetSource('audio/empty_click.wav'));
           }
           if (_heart == 0) {
             _controller.stop();
-            _showDialog();
+            _showFinishDialog();
           }
         });
       });
@@ -82,7 +87,7 @@ class _GamePageState extends State<GamePage>
 
   void _onTapDown(TapDownDetails details) {
     if (details.localPosition.dx >= _left &&
-        details.localPosition.dx <= _left + 80.0 &&
+        details.localPosition.dx <= _left + 80 &&
         details.localPosition.dy >= _top &&
         details.localPosition.dy <= _top + 80.0) {
       setState(() {
@@ -93,10 +98,28 @@ class _GamePageState extends State<GamePage>
         _left = _random.nextDouble() * MediaQuery.of(context).size.width;
         // _right = _random.nextDouble() * MediaQuery.of(context).size.width;
         _randomIndex = random.nextInt(imageList.length);
+        _calculatedScore = _score * _fallingSpeed.round();
       });
       _animation;
       _player.play(AssetSource('audio/target_click.wav'));
+      _backgroundImageChanger();
     }
+  }
+
+  void _onTapPause() {
+    _controller.stop();
+    setState(() {
+      _stopped = true;
+      _showPauseDialog();
+    });
+  }
+
+  void _onTapPlay() {
+    Navigator.of(context, rootNavigator: true).pop();
+    _controller.repeat();
+    setState(() {
+      _stopped = false;
+    });
   }
 
   void _restartGame() {
@@ -105,13 +128,28 @@ class _GamePageState extends State<GamePage>
       Random random = Random();
       _heart = 5;
       _top = -80;
-      _left = _random.nextDouble() * MediaQuery.of(context).size.width - 40;
+      _left = (_random.nextDouble() * MediaQuery.of(context).size.width) - 40;
       _randomIndex = random.nextInt(imageList.length);
     });
     _controller.repeat();
+    _try -= 1;
   }
 
-  void _showDialog() {
+  void _backgroundImageChanger() {
+    setState(() {
+      if (_calculatedScore > 1000) {
+        _backgroundImg = 'lib/assets/bg_2.gif';
+      }
+      if (_calculatedScore > 2000) {
+        _backgroundImg = 'lib/assets/bg_1.gif';
+      }
+      if (_calculatedScore > 3000) {
+        _backgroundImg = 'lib/assets/bg_3.gif';
+      }
+    });
+  }
+
+  void _showFinishDialog() {
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -119,7 +157,10 @@ class _GamePageState extends State<GamePage>
         return AlertDialog(
           elevation: 7,
           shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30.0))),
+            borderRadius: BorderRadius.all(
+              Radius.circular(26.0),
+            ),
+          ),
           title: Text(
             'GAME IS OVER!',
             style: GoogleFonts.righteous(
@@ -131,16 +172,39 @@ class _GamePageState extends State<GamePage>
               ),
             ),
           ),
-          content: Text(
-            'YOUR SCORE: $_score',
-            style: GoogleFonts.righteous(
-              textStyle: const TextStyle(
-                color: Colors.black,
-                letterSpacing: .5,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+          content: Row(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'YOUR SCORE: $_calculatedScore',
+                    style: GoogleFonts.righteous(
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                        letterSpacing: .5,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  if (_try > 0)
+                    Text(
+                      'You can try $_try more',
+                      style: GoogleFonts.righteous(
+                        textStyle: const TextStyle(
+                          color: Colors.black,
+                          letterSpacing: .5,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                ],
               ),
-            ),
+            ],
           ),
           actionsAlignment: MainAxisAlignment.center,
           actions: <Widget>[
@@ -160,31 +224,133 @@ class _GamePageState extends State<GamePage>
                 elevation: 0,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(18.0)),
-                minimumSize: const Size(80, 40),
+                minimumSize: const Size(100, 40),
+              ),
+              child: const Text(
+                'Exit',
+              ),
+            ),
+            if (_try > 0) const SizedBox(width: 8.0),
+            if (_try > 0)
+              ElevatedButton(
+                onPressed: _restartGame,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 206, 3, 95),
+                  // shadowColor: Colors.red,
+                  elevation: 7,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0)),
+                  minimumSize: const Size(100, 40),
+                ),
+                child: const Text(
+                  'Continue',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 40,
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void _showPauseDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          elevation: 7,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(26.0),
+            ),
+          ),
+          title: Text(
+            'GAME PAUSED!',
+            style: GoogleFonts.righteous(
+              textStyle: const TextStyle(
+                color: Colors.black,
+                letterSpacing: .5,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          content: Row(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Wait... Take a rest! You can do it...',
+                    style: GoogleFonts.righteous(
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                        letterSpacing: .5,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actionsAlignment: MainAxisAlignment.center,
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: ((context) => const StartPage()),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 57, 210, 192),
+                // shadowColor: Colors.red,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)),
+                minimumSize: const Size(100, 40),
               ),
               child: const Text(
                 'Exit',
               ),
             ),
             ElevatedButton(
-              onPressed: _restartGame,
+              onPressed: _onTapPlay,
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 206, 3, 95),
                 // shadowColor: Colors.red,
                 elevation: 7,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(18.0)),
-                minimumSize: const Size(120, 40),
+                minimumSize: const Size(100, 40),
               ),
               child: const Text(
                 'Continue',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                  fontSize: 14,
                   color: Colors.white,
                 ),
               ),
             ),
+            const SizedBox(
+              height: 40,
+            )
           ],
         );
       },
@@ -194,10 +360,9 @@ class _GamePageState extends State<GamePage>
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         image: DecorationImage(
-            image: AssetImage('lib/assets/backgroundCity.gif'),
-            fit: BoxFit.cover),
+            image: AssetImage(_backgroundImg), fit: BoxFit.cover),
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
@@ -207,15 +372,16 @@ class _GamePageState extends State<GamePage>
             alignment: Alignment.center,
             children: <Widget>[
               Positioned(
-                  top: _top,
-                  left: _left,
-                  child: Image.asset(imageList[_randomIndex],
-                      width: _width, height: _height)),
+                top: _top,
+                left: _left,
+                child: Image.asset(imageList[_randomIndex],
+                    width: _width, height: _height),
+              ),
               Positioned(
                 top: 88.0,
                 left: 20.0,
                 child: Text(
-                  'Score: $_score',
+                  'Score: $_calculatedScore',
                   style: GoogleFonts.righteous(
                     textStyle: const TextStyle(
                       color: Colors.white,
@@ -262,6 +428,32 @@ class _GamePageState extends State<GamePage>
                   ),
                 ),
               ),
+              if (!_stopped)
+                Positioned(
+                  top: 64,
+                  right: 10,
+                  child: IconButton(
+                    icon: const Icon(Icons.pause),
+                    onPressed: () {
+                      _onTapPause();
+                    },
+                    iconSize: 30,
+                    color: Color.fromARGB(255, 206, 3, 95),
+                  ),
+                ),
+              if (_stopped)
+                Positioned(
+                  top: 64,
+                  right: 10,
+                  child: IconButton(
+                    icon: const Icon(Icons.play_arrow),
+                    onPressed: () {
+                      _onTapPlay();
+                    },
+                    iconSize: 30,
+                    color: Color.fromARGB(255, 206, 3, 95),
+                  ),
+                ),
             ],
           ),
         ),
